cd16a408fe7a700de5c4779406819b36a81f7485
mohit <mohitklamba@gmail.com>
2021-04-25 02:00:58 +0530

    work started

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+function navbar() {
+    return (
+        <AppBar title="Random Image Finder"/>
+    )
+}
+
+export default navbar
